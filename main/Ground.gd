6467ba3b912d372b28b74264dff31b10e280aeb0
Lueegee1 <siriusvoss@gmail.com>
2025-11-03 10:52:20 +0100

    Attach ground script

--- main/Ground.gd
+++ main/Ground.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var increment = 2000
+var lev1 = increment
+var lev2 = 2*increment
+var lev3 = 3*increment
 
 func _ready() -> void:
 	pass
@@ -8,4 +12,6 @@ func _process(delta: float) -> void:
 	queue_redraw()
 	
 func _draw() -> void:
-	draw_rect(Rect2(0,0,2000,2000),Color.WHITE,true)
+	draw_rect(Rect2(0,0,lev1,lev1),Color.WHITE,true)
+	draw_rect(Rect2(lev1,lev1,lev2,lev2),Color.AQUAMARINE,true)
+	draw_rect(Rect2(lev2,lev2,lev3,lev3),Color.MEDIUM_SPRING_GREEN,true)
