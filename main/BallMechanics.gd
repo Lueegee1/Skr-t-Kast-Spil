4fbc85d8a5843018fc97f0855e60ba938fd6f706
CoinLords <eliot.rc@outlook.fr>
2025-11-03 11:18:18 +0100

    Update BallMechanics.gd

--- main/BallMechanics.gd
+++ main/BallMechanics.gd
@@ -1,16 +1,20 @@
 extends RigidBody2D
 
-@export var launch_speed: float = 500.0 # speed that the RigidBody2D is launched with
-@export var launch_angle: float = 45.0  # angle of launch of the RigidBody2D
-@export var stop_threshold := 5.0       # velocity threshold rquired for the RigidBody2D to be considered stopped
-@export var stop_delay := 1.5           # seconds it must stay still to count as stopped
+@export var launch_speed: float = 500.0      # speed that the RigidBody2D is launched with
+@export var launch_angle: float = 45.0       # angle of launch of the RigidBody2D
+@export var stop_threshold := 0.0            # velocity threshold rquired for the RigidBody2D to be considered stopped
+@export var stop_delay := 1.5                # seconds it must stay still to count as stopped
 
-@onready var StartPos = $"../Marker2D"  # start position of the RigidBody2D
+@onready var StartPos = $"../Marker2D"       # start position of the RigidBody2D
+@onready var Sprite = $Sprite2D              # sprite of the RigidBody2D
+@onready var collision = $CollisionPolygon2D # collision of the RigidBody2D
 
-var still_time := 0.0                   # var to hold the ellapsed time the RigidBody2D has been stationary
+var still_time := 0.0                        # var to hold the ellapsed time the RigidBody2D has been stationary
 
 func _ready() -> void:
-	LaunchRigidBody(launch_angle)
+# launches the polygon soon after running the scene
+	get_tree().create_timer(1.0)
+	LaunchRigidBody(launch_angle) 
 
 func LaunchRigidBody(angle):
 	var radians = deg_to_rad(angle)
