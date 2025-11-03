f0eeff65f5a6b954ec5713027c1331ba9f3864cd
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-11-03 11:04:00 +0100

    More variables

--- main/Global.gd
+++ main/Global.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var exp = 0
 var money = 0
+var prestige = 0
 
 var GameController
--- main/Ui_And_Mainscript.gd
+++ main/Ui_And_Mainscript.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 @onready var Camera = $Camera2D
 @onready var RigidBody = $LaunchSite/RigidBody2D
+@onready var XpLabel = $UI/Camera2D/XpLabel
+@onready var MoneyLabel = $UI/Camera2D/MoneyLabel
+@onready var PrestigeLabel = $UI/Camera2D/PrestigeLabel
 	
 func _process(_delta: float):
 	Camera.global_position = RigidBody.global_position
+	XpLabel.text = Global.exp
+	MoneyLabel.text = Global.money
+	PrestigeLabel.text = Global.prestige
+	
