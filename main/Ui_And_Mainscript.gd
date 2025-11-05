738a40bb730dbcf4e3a27a71c2c7bd4c63d4cb82
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-11-05 13:23:15 +0100

    UPDATED POSITIONS

--- main/Ui_And_Mainscript.gd
+++ main/Ui_And_Mainscript.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 @onready var Camera = $UI/Camera2D
 @onready var RigidBody = $LaunchSite/RigidBody2D
-@onready var XpLabel = $UI/Camera2D/XpLabel
-@onready var MoneyLabel = $UI/Camera2D/MoneyLabel
-@onready var PrestigeLabel = $UI/Camera2D/PrestigeLabel
-var offsetXP = Vector2(-500,0)
-var offsetMoney = Vector2(-250,0)
+@onready var XpLabel = $UI/Camera2D/CanvasLayer/XpLabel
+@onready var MoneyLabel = $UI/Camera2D/CanvasLayer/MoneyLabel
+@onready var PrestigeLabel = $UI/Camera2D/CanvasLayer/PrestigeLabel
+var offsetXP = Vector2(0,0)
+var offsetMoney = Vector2(0,0)
 var offsetPrestige = Vector2(0,0)
 	
 func _process(_delta: float):
@@ -14,7 +14,7 @@ func _process(_delta: float):
 	XpLabel.text = str(Global.exp)
 	MoneyLabel.text = str(Global.money)
 	PrestigeLabel.text = str(Global.prestige)
-	XpLabel.global_position = Camera.global_position + offsetXP
-	MoneyLabel.global_position = Camera.global_position + offsetMoney
-	PrestigeLabel.global_position = Camera.global_position+ offsetPrestige
+	XpLabel.position = offsetXP
+	MoneyLabel.position = offsetMoney
+	PrestigeLabel.position = offsetPrestige
 	
