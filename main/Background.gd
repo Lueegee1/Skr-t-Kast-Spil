448269bc2d6466efd9c15feafefb729090a9ff72
CoinLords <eliot.rc@outlook.fr>
2025-11-05 13:31:50 +0100

    call me daddy

--- main/Background.gd
+++ main/Background.gd
@@ -6,7 +6,7 @@ extends ColorRect
 
 func _process(_delta):
 	var mat = grid.material as ShaderMaterial
+	var screen_top_left = camera.get_screen_center_position() - (get_viewport_rect().size / 2) * camera.zoom
 
-	# Pass ball position and camera offset to shader
+	mat.set_shader_parameter("camera_origin", screen_top_left)
 	mat.set_shader_parameter("ball_position", ball.global_position)
-	mat.set_shader_parameter("world_offset", camera.get_screen_center_position() - camera.offset)
