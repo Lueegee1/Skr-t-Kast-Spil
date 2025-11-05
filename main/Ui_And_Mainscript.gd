7a18a8f9956dbb5f4ee40db7cc6bd21ca0daab52
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-11-05 13:07:42 +0100

    POSITION

--- main/Ui_And_Mainscript.gd
+++ main/Ui_And_Mainscript.gd
@@ -5,8 +5,8 @@ extends Node2D
 @onready var XpLabel = $UI/Camera2D/XpLabel
 @onready var MoneyLabel = $UI/Camera2D/MoneyLabel
 @onready var PrestigeLabel = $UI/Camera2D/PrestigeLabel
-var offsetXP = Vector2(0,0)
-var offsetMoney = Vector2(0,0)
+var offsetXP = Vector2(-500,0)
+var offsetMoney = Vector2(-250,0)
 var offsetPrestige = Vector2(0,0)
 	
 func _process(_delta: float):
@@ -14,7 +14,7 @@ func _process(_delta: float):
 	XpLabel.text = str(Global.exp)
 	MoneyLabel.text = str(Global.money)
 	PrestigeLabel.text = str(Global.prestige)
-	XpLabel.global_position = RigidBody.global_position + offsetXP
-	MoneyLabel.global_position = RigidBody.global_position + offsetMoney
-	PrestigeLabel.global_position = RigidBody.global_position + offsetPrestige
+	XpLabel.global_position = Camera.global_position + offsetXP
+	MoneyLabel.global_position = Camera.global_position + offsetMoney
+	PrestigeLabel.global_position = Camera.global_position+ offsetPrestige
 	
