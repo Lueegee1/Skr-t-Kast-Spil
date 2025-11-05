1e11def5bf92686de743bc8ed322c8f44fe57760
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-11-05 15:08:08 +0100

    UPGRADES 1.2

--- main/Ui_And_Mainscript.gd
+++ main/Ui_And_Mainscript.gd
@@ -7,6 +7,7 @@ extends Node2D
 @onready var MoneyLabel = $UI/Camera2D/CanvasLayer/MoneyLabel
 @onready var PrestigeLabel = $UI/Camera2D/CanvasLayer/PrestigeLabel
 @onready var Upgrades = $UI/Camera2D/CanvasLayer/VBoxContainer
+@onready var angle = $UI/Camera2D/CanvasLayer/VBoxContainer/Angle
 var possible_angles = [45,45]
 
 
@@ -14,6 +15,7 @@ var offsetXP = Vector2(0,0)
 var offsetMoney = Vector2(200,0)
 var offsetPrestige = Vector2(400,0)
 var offsetUpgrades = Vector2(800,0)
+var angle_price = 5
 	
 func _process(_delta: float):# update position and money, xp and prestige
 	Camera.global_position = RigidBody.global_position #updates camera position
@@ -32,23 +34,29 @@ func _on_angle_changed(value: float):
 
 
 func _on_angle_upgrade_pressed() -> void:
-	pass # Replace with function body.
+	if Global.money > angle_price:
+		if angle.min_value > 0 and angle.max_value < 90:
+			angle.min_value = angle.min_value - 5
+			angle.max_value = angle.max_value + 5
+		
 
 
 func _on_gravity_upgrade_pressed() -> void:
-	pass # Replace with function body.
+	if Global.Ball.gravity > 0.05:
+		Global.Ball.gravity = Global.Ball.gravity - 0.05
+	
 
 
 func _on_friction_upgrade_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_elasticity_upgrade_pressed() -> void:
-	pass # Replace with function body.
+func _on_elasticity_upgrade_pressed():
+	Global.Ball.ground_elasticity = Global.Ball.ground_elasticity + 5
 
 
 func _on_speed_upgrade_pressed() -> void:
-	pass # Replace with function body.
+	Global.Ball.launch_speed = Global.Ball.launch_speed * 1.05
 
 
 func _on_air_resistance_upgrade_pressed() -> void:
