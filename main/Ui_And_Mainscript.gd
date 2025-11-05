2449487d8e38489694749c1862edb491a317e4de
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-11-05 12:47:34 +0100

    lABEL RELATIVE TO RIGIDBODY

--- main/Ui_And_Mainscript.gd
+++ main/Ui_And_Mainscript.gd
@@ -5,10 +5,16 @@ extends Node2D
 @onready var XpLabel = $UI/Camera2D/XpLabel
 @onready var MoneyLabel = $UI/Camera2D/MoneyLabel
 @onready var PrestigeLabel = $UI/Camera2D/PrestigeLabel
+var offsetXP = Vector2(0,0)
+var offsetMoney = Vector2(0,0)
+var offsetPrestige = Vector2(0,0)
 	
 func _process(_delta: float):
 	Camera.global_position = RigidBody.global_position
 	XpLabel.text = str(Global.exp)
 	MoneyLabel.text = str(Global.money)
 	PrestigeLabel.text = str(Global.prestige)
+	XpLabel.global_position = RigidBody.global_position + offsetXP
+	MoneyLabel.global_position = RigidBody.global_position + offsetMoney
+	PrestigeLabel.global_position = RigidBody.global_position + offsetPrestige
 	
